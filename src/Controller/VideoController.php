df2e020625d96ac217e770c9dc199a06221bbe74
crystalcleartechnologies <admin@crystalclearfiber.com>
2015-07-17 09:31:16 -0500

    Initial Commit

---
+++ src/Controller/VideoController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\Network\Exception\NotFoundException;
+use Cake\View\Exception\MissingTemplateException;
+use Cake\Controller\Controller;
+
+class VideoController extends AppController
+{
+    public function index()
+    {
+
+    }
+    //return $this->redirect('/videos.ctp');
+    /*public function initialize(){
+        return $this->redirect('http://www.example.com');
+    }
+
+    public function registration(){
+        return $this->redirect('http://www.example.com');s
+    }*/
+}
+
+?>
