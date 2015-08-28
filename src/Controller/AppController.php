f7406d59bdc126001cd84a5475c48a731588b8e6
Zachary <zach@crystalclearfiber.com>
2015-08-28 16:16:03 -0500

    Basic Auth added

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -38,6 +38,21 @@ class AppController extends Controller
     {
         parent::initialize();
         $this->loadComponent('Flash');
+        $this->loadComponent('Auth', [
+            'loginRedirect' => [
+                'controller' => 'Articles',
+                'action' => 'index'
+            ],
+            'logoutRedirect' => [
+                'controller' => 'Pages',
+                'action' => 'display',
+                'home'
+            ]
+        ]);
+    }
+    public function beforeFilter(Event $event)
+    {
+    $this->Auth->allow(['index', 'view', 'display']);
     }
 }
 
